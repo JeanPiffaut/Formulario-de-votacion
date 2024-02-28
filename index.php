77ba0d9a4c808d8979e07c484eb7c34ca6800577
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-02-28 15:29:44 -0300

    detalles

--- index.php
+++ index.php
@@ -18,14 +18,14 @@ $candidates_result = $conn->query($sql);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulario de prueba Desis</title>
+    <title>Formulario de Votación</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <div class="container mt-5">
     <div class="card">
         <div class="card-body">
-            <h1 class="mb-4">Formulario de Registro</h1>
+            <h1 class="mb-4">Formulario de Votación</h1>
             <form action="/procesar_formulario" id="formToVote" method="post" onsubmit="if(validateForm()) {sendForm();}return false;">
                 <div class="mb-3">
                     <label for="name" class="form-label">Nombre y Apellido</label>
