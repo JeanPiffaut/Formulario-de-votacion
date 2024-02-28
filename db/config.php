126b37e48ea1cba1c862e312a15c8b58262796ac
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-02-28 12:28:14 -0300

    config db

--- db/config.php
+++ db/config.php
@@ -6,13 +6,17 @@ $user = "root";
 $pass = "root";
 $db_name = "vote_db";
 
-// Conexión a la base de datos
-$conn = new mysqli($host, $user, $pass, $db_name);
+try {
+    // Conexión a la base de datos
+        $conn = new mysqli($host, $user, $pass, $db_name);
 
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error de conexión a la base de datos: " . $conn->connect_error);
+    // Verificar la conexión
+        if ($conn->connect_error) {
+            die("Error de conexión a la base de datos: " . $conn->connect_error);
+        }
+
+    // Configurar el conjunto de caracteres a UTF-8
+        $conn->set_charset("utf8");
+} catch (Exception $e) {
+
 }
-
-// Configurar el conjunto de caracteres a UTF-8
-$conn->set_charset("utf8");
